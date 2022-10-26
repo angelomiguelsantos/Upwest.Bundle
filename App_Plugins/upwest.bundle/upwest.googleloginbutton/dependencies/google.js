8bb50e9e0b12ad2bcecdb6d0af842b2bc74d8636
angelomiguelsantos <angelomiguelsantos@gmail.com>
2022-10-26 10:16:29 +0100

    Add files via upload

---
+++ App_Plugins/upwest.bundle/upwest.googleloginbutton/dependencies/google.js
@@ -0,0 +1,41 @@
+﻿function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+}
+
+function handleCredentialResponse(response) {    
+    const responsePayload = parseJwt(response.credential);
+    $.ajax({
+        url: "/umbraco/api/GoogleLoginButton/Login",
+        data: JSON.stringify({ email: responsePayload.email, name: responsePayload.name, languageId: googleLanguageId, loginTimeout: googleLoginTimeout, redirectTo: googleRedirectTo, contentId: googleContentId }),        
+        headers: { "RequestVerificationToken": googleButtonToken },
+        type: 'POST',
+        cache: false,
+        dataType: "json",
+        async: true,
+        contentType: 'application/json; charset=utf-8',
+        beforeSend: function () {
+            $(".box").show();
+        },
+        success: function (data) {
+            if (data.action === "newexternallogin") {
+                window.location.href = data.url + "?google=true";
+            }
+            else {
+                window.location.href = data.url;
+            }
+        },
+        error: function (data) {
+            toastr.error(data.responseJSON.message);
+            $(".box").fadeOut();
+        },
+        complete: function (data) {
+
+        }
+    });
+}
---
+++ App_Plugins/upwest.bundle/upwest.logoutbutton/dependencies/upwest.logoutButton.js
@@ -0,0 +1,98 @@
+﻿
+
+
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/" + facebookLanguage + "/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: facebookKey,
+        cookie: true,
+        xfbml: true,
+        version: 'v' + facebookVersion
+    });
+}
+
+$(document).ready(function () {
+    $(".btn-facebook").click(function () {
+        $(".box").show();
+        FB.logout(function () {
+            deleteFacebookCookie($(this).data("key"));
+        });
+        logout(this);
+    });
+
+    $(".btn-google").click(function () {
+        google.accounts.id.disableAutoSelect();
+        logout(this);
+    });
+
+    $(".btn-site").click(function () {        
+        logout(this);
+    });
+
+    $(".btn-linkedin").click(function () {
+        logout(this);
+    });
+
+    $(".btn-twitter").click(function () {
+        logout(this);
+    });
+
+    $(".btn-instagram").click(function () {
+        logout(this);
+    });
+
+});
+
+function logout(e) {
+
+    var url = $(e).data("url");
+
+    $.ajax({
+        url: "/umbraco/api/LogoutButton/Logout",
+        headers: { "RequestVerificationToken": $(e).data("token") },        
+        type: 'POST',
+        data: JSON.stringify({ languageId: $(e).data("language") }),
+        cache: false,
+        dataType: "json",
+        async: true,           
+        beforeSend: function () {
+            $(".box").show();
+        },
+        success: function () {
+            document.location.href = url;
+        },
+        error: function (data) {
+
+            var done = false;
+            try {
+                if (data.responseJSON.message) {
+                    toastr.error(data.responseJSON.message);
+                    done = true;
+                }
+            } catch (e) { }
+
+
+            if (!done) {
+                try {
+                    if (data.responseText) {
+                        toastr.error(data.responseText);
+                    }
+                } catch (e) { }
+            }
+            $(".box").fadeOut();
+        }
+    });
+}
+
+function deleteFacebookCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
