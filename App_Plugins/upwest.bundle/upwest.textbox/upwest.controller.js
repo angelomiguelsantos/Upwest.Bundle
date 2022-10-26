e0e2c41cf43e89050a92a6eea3436f7071edf7a2
angelomiguelsantos <angelomiguelsantos@gmail.com>
2022-10-26 10:23:22 +0100

    Add files via upload

---
+++ App_Plugins/upwest.bundle/upwest.textbox/upwest.controller.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    function textboxController($scope) {
+        var vm = this;
+        vm.alias = $scope.model.alias;
+        vm.required = $scope.model.validation.mandatory;
+        vm.message = $scope.model.validation.mandatoryMessage;
+        vm.icon = $scope.model.config.icon;
+        vm.maxLength = $scope.model.config.maxLength; 
+    }
+
+    angular.module('umbraco').controller('upwest.textbox', textboxController);
+
+})();
+
+
+
+
+
+
+
+
