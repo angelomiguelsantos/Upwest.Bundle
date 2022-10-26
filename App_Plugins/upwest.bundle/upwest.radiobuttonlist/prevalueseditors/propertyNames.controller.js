66896865f7e7f98f1f4d6a0af3b09e469cffb1aa
angelomiguelsantos <angelomiguelsantos@gmail.com>
2022-10-26 10:21:02 +0100

    Add files via upload

---
+++ App_Plugins/upwest.bundle/upwest.radiobuttonlist/prevalueseditors/propertyNames.controller.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    'use strict';
+    function propertyNamesController($http, $scope) {
+
+        var vm = this;
+        vm.alias = $scope.model.alias;        
+        $("#" + vm.alias).val($scope.model.value);
+        $scope.propertyNames = [];
+        $scope.retreivePropertyNames = function () {
+            $http.get("/umbraco/backoffice/api/CheckboxList/GetPropertyNames").then(function (data) {
+                $scope.propertyNames = data.data;
+            });
+        };
+
+        $scope.retreivePropertyNames();
+
+        $scope.$on("formSubmitting", function () {            
+            $scope.model.value = $("#" + vm.alias).val();
+        });
+
+    }
+    angular.module('umbraco').controller('upwest.PropertyNames', propertyNamesController);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
