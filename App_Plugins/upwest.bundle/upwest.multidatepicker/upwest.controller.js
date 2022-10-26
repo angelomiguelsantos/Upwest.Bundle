e0266ee5444d73beedd0c8a729b90010356bfeba
angelomiguelsantos <angelomiguelsantos@gmail.com>
2022-10-26 10:19:35 +0100

    Add files via upload

---
+++ App_Plugins/upwest.bundle/upwest.multidatepicker/upwest.controller.js
@@ -0,0 +1,63 @@
+﻿var dateRangePickerFormat = "d-m-Y";
+(function () {
+    'use strict';
+
+    function multiDatePickerController($http, $scope, $routeParams, assetsService,userService) {
+
+        var vm = this;
+        vm.alias = $scope.model.alias;
+        vm.required = $scope.model.validation.mandatory;
+        vm.message = $scope.model.validation.mandatoryMessage;        
+        var localeScript = ""; 
+
+        if ($routeParams.section === 'settings') {
+            return;
+        }
+
+        var user = userService.getCurrentUser().then(function (user) {
+            if (user.locale != "en-US") {                
+                localeScript = "~/App_Plugins/upwest.bundle/upwest.multidatepicker/dependencies/locales/bootstrap-datepicker." + user.locale.slice(0, 2) + ".min.js";
+            }
+
+            assetsService
+                .load([                    
+                    "~/App_Plugins/upwest.bundle/upwest.multidatepicker/dependencies/bootstrap-datepicker.min.js",
+                    localeScript
+                ])
+                .then(function () {
+                    setTimeout(() => {
+                        $('#' + vm.alias).datepicker({
+                            multidate: true,
+                            todayHighlight: true,
+                            language: user.locale.slice(0, 2),
+                            format: $scope.model.config.format.toLowerCase(),
+                            calendarWeeks: $scope.model.config.calendarWeeks == 1 ? true : false,
+                            multidateSeparator: ",",
+                            todayBtn: $scope.model.config.todayBtn == 1 ? true : false,
+                            clearBtn: $scope.model.config.clearBtn == 1 ? true : false
+                        });
+                    }, "1");
+                });
+        });
+
+        vm.iconClick = iconClick;
+
+        function iconClick() {
+            $('#' + vm.alias).focus();
+        }
+
+
+
+    }
+
+    angular.module('umbraco').controller('multiDatePicker', multiDatePickerController);
+
+})();
+
+
+
+
+
+
+
+
---
+++ App_Plugins/upwest.bundle/upwest.numeric/upwest.controller.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    function numericController($scope,$routeParams) {
+        var vm = this;
+
+        if ($routeParams.section === 'settings') {
+            return;
+        }
+
+
+        vm.alias = $scope.model.alias;
+        vm.required = $scope.model.validation.mandatory;
+        vm.message = $scope.model.validation.mandatoryMessage;
+        vm.icon = $scope.model.config.icon;        
+        vm.step = $scope.model.config.step;        
+        vm.max = $scope.model.config.max;        
+        vm.min = $scope.model.config.min;        
+        setTimeout(() => {
+            $("#" + vm.alias).val($scope.model.value);
+        }, 100);
+    }
+
+    angular.module('umbraco').controller('upwest.numeric', numericController);
+
+})();
+
+
+
+
+
+
+
+
